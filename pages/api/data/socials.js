3d988d4589f1bb5eb2af358830689f121ff398f2
AlexSidol <asidolaka@gmail.com>
2023-03-06 18:48:20 +0200

    Update social.js
    Update social.js

--- pages/api/data/socials.js
+++ pages/api/data/socials.js
@@ -1,27 +1,18 @@
 export const socials = [
   {
     id: 1,
-    icon: 'youtube',
-    path: 'https://youtube.com/YauhenKavalchuk',
+    icon: 'linkedin',
+    path: 'https://www.linkedin.com/in/alesia-sidolaka/',
   },
   {
     id: 2,
     icon: 'instagram',
-    path: 'https://instagram.com/YauhenKavalchuk',
+    path: 'https://www.instagram.com/p/BorbODyj7HrmaU9-tpQMWYj_0Eeqy4t9SuAm180/',
   },
+
   {
     id: 3,
-    icon: 'linkedin',
-    path: 'https://linkedin.com/in/YauhenKavalchuk',
-  },
-  {
-    id: 4,
-    icon: 'vk',
-    path: 'https://vk.com/YauhenKavalchuk',
-  },
-  {
-    id: 5,
     icon: 'twitter',
-    path: 'https://twitter.com/YauhenKavalchuk',
+    path: 'https://twitter.com/asidolaka_sid',
   },
 ];
